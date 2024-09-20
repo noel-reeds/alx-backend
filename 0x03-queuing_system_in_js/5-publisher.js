aa33625d777202fe2090b5aa1b6412d8324a5b76
Reeds <noelreeds01@gmail.com>
2024-09-20 12:39:18 +0000

    publishes messages to redis client channel

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -1,17 +1,16 @@
 import { createClient } from 'redis';
 
-const client = createClient()
-	.on('connect', () => console.log('Redis client connected to the server'))
-	.on('error', (err) => console.log(`Redis client not connected to the server: ${err.message}`));
+const client = createClient();
+	
+client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('error', (err) => console.log(`Redis client not connected to the server: ${err.message}`));
 
-const publishMessage = (channel, message) => {
-	if (err){
-		console.error(err);
-	} else {
-		connectTimeout: time
+const publishMessage = (message, time) => {
+	setTimeout(() => {
 		console.log(`About to send ${message}`);
-		}
-	});
+		client.publish('holberton school channel', message);
+}
+
 
 
 publishMessage("Holberton Student #1 starts course", 100);
