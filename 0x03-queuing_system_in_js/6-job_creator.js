ca1f4306bbe6a48c6215f53d00eb4730c8d73ddd
Reeds <noelreeds01@gmail.com>
2024-09-21 15:05:04 +0000

    creates a job

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,17 @@
+const { kue } = require('kue');
+
+const queue = kue.createQueue();
+
+const job = queue.create('push_notification_code', {
+	phoneNumber: string,
+	message: string,
+	}).save( function( err ){
+		if( !err ) console.log(`Notification job created: ${job.id}`);
+	});
+
+job.on('complete', (result) => {
+	console.log('Notification job completed');
+
+}).on('failed', (errMessage) => {
+	console.log('Notification job failed');
+});
