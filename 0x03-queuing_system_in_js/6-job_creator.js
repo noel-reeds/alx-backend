b06ad4390be273ee676d236cd8361853412d9db4
Reeds <noelreeds01@gmail.com>
2024-09-24 13:50:38 +0000

    added job data

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ import kue from 'kue';
 const queue = kue.createQueue();
 
 const job = queue.create('push_notification_code', {
-	phoneNumber: "",
-	message: "",
+	phoneNumber: "415-3518-780",
+	message: "This is the code to verify your account",
 	}).save( function( err ){
 		if( !err ) console.log(`Notification job created: ${job.id}`);
 	});
