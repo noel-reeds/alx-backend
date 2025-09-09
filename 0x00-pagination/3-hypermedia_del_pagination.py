d2d82582dd72885445e6a3999ed2a542bc20417c
altoids <noelreeds01@gmail.com>
2025-09-10 01:18:08 +0300

    retrieves data after deletions

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,8 +40,7 @@ class Server:
         assert type(page_size) is int and page_size > 0
         lst = self.dataset()
         start, end = index_range(page, page_size)
-        ln_lst = len(lst)
-        if end > ln_lst:
+        if end > (ln_lst := len(lst))
             return []
         return lst[start:end]
 
