de8bfc690c7de1c13c8b262ec9cd2d15c9e58087
altoids <noelreeds01@gmail.com>
2025-09-12 17:27:34 +0300

    implements mru caching system

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""LRU Caching"""
+from collections import defaultdict
+from functools import wraps
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Implements a Least Recently Used algorithm"""
+    def __init__(self):
+        """Invoke base class init method"""
+        BaseCaching.__init__(self)
+        self.mru = None
+
+    def put(self, key, item):
+        """Assigns key to item in cache"""
+        cache = self.cache_data
+        if key and item:
+            if key in self.cache_data.keys():
+                cache[key] = item
+                return
+            elif len(cache) >= BaseCaching.MAX_ITEMS:
+                # discard the most recently used - mru
+                if self.mru and self.mru in cache.keys():
+                    cache.pop(self.mru)
+                    print(f"DISCARD: {self.mru}")
+                else:
+                    # if None, remove first item in cache
+                    cache.popitem()
+            cache[key] = item
+
+    def counted(fn):
+        """Keeps track of get fn calls"""
+        @wraps(fn)
+        def wrapper(self, *args, **kwargs):
+            keys = args[0]
+            if keys in self.cache_data.keys():
+                self.mru = keys
+            return fn(self, *args, **kwargs)
+        return wrapper
+
+    @counted
+    def get(self, key):
+        """return the value linked to key"""
+        if key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
