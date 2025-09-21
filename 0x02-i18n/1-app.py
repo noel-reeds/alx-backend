dae5402eff667b6d960959ff94d7bebd44a374d4
altoids <noelreeds01@gmail.com>
2025-09-22 02:47:26 +0300

    specifies configs for flask app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python3
-"""Basic Flask app"""
-from flask import Flask, render_template
-from flask_babel import Babel
-from werkzeug.utils import import_string
+"""Basic Babel setup"""
 
 
-app = Flask(__name__)
-cfg = import_string('config.Config')()
-app.config.from_object(cfg)
-babel = Babel(app)
-
-
-@app.route('/')
-def index():
-    """renders index files"""
-    return render_template("0-index.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+class Config(object):
+    """configurations"""
+    LANGUAGES = ["en", "fr"]
+    DEFAULT_LOCALE = "en"
+    DEFAULT_TIMEZONE = "UTC"
