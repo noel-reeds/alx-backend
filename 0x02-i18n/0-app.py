df388308c55824998da701bb51272fe4ef310257
altoids <noelreeds01@gmail.com>
2025-09-22 02:46:51 +0300

    configures app with defaults timezone

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 from flask import Flask, render_template
+from flask_babel import Babel
+from werkzeug.utils import import_string
+
+
+cfg = import_string('1-app.Config')()
 app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(cfg)
 
 
 @app.route('/')
