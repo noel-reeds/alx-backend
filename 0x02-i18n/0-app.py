1725bb06e40030cc551466552f30251be429f624
altoids <noelreeds01@gmail.com>
2025-09-22 03:01:00 +0300

    sets up a basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 from flask import Flask, render_template
-from flask_babel import Babel
-from werkzeug.utils import import_string
 
 
-cfg = import_string('1-app.Config')()
 app = Flask(__name__)
-babel = Babel(app)
-app.config.from_object(cfg)
 
 
 @app.route('/')
 def index():
     """renders index files"""
     return render_template("0-index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
