f118b8a152d78bf3a18aab99af327a2bdeacb92a
altoids <noelreeds01@gmail.com>
2025-09-22 14:32:41 +0300

    uses @babel.localeselector

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -7,13 +7,10 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
-
+@babel.localeselector
 def get_locale():
     """Retrieves lang-setting"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route("/")
