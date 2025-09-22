fb96cb65a8a59c85a890850afd92892972d0651d
altoids <noelreeds01@gmail.com>
2025-09-22 03:01:31 +0300

    app i18n configs

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 """Basic Babel setup"""
+from flask_babel import Babel
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+babel = Babel(app)
 
 
 class Config(object):
@@ -7,3 +13,5 @@ class Config(object):
     LANGUAGES = ["en", "fr"]
     DEFAULT_LOCALE = "en"
     DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object('1-app.Config')
