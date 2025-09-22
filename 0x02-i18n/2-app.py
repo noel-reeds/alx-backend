2d4cd0dcaec36aedc11d3f70837a8932aec595c0
altoids <noelreeds01@gmail.com>
2025-09-22 03:28:05 +0300

    fix: 500 error

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Get locale from request"""
+from flask import request, Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+def get_locale():
+    """Retrieves lang-setting"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route("/")
+def index():
+    """Renders a page"""
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
