e97a7925cca9f15cdbbc03eabefa44a06e322af4
altoids <noelreeds01@gmail.com>
2025-09-23 16:01:35 +0300

    specifies lang config from 1-app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,8 +2,12 @@
 """Get locale from request"""
 from flask import request, Flask, render_template
 from flask_babel import Babel
-app = __import__('1-app').app
-babel = __import__('1-app').babel
+conf = __import__('1-app').Config
+
+
+app = Flask(__name__)
+app.config.from_object(conf)
+babel = Babel(app)
 
 
 @babel.localeselector
